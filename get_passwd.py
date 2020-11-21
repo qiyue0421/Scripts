e0808f693e04c1a32f67cfb2a5ca5b0cd0fe58c1
qiyue0421 <qiyue0421@gmail.com>
2020-11-21 11:59:01 +0800

    生成高强度随机密码

---
+++ get_passwd.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+
+passwds = []
+count = input('请确认要生成几条密码： ')
+slat = '!@#$%^&*'
+while True:
+    passwd = set(random.sample(string.ascii_letters + string.digits + slat, 8))
+    if passwd.intersection(string.ascii_uppercase) and passwd.intersection(string.ascii_lowercase) and passwd.intersection(string.digits) and passwd.intersection(slat):
+        passwds.append(''.join(passwd))
+    if len(passwds) == int(count):
+        break
+
+print(passwds)
